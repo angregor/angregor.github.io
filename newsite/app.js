a7184feeb5bf91bf6499796886f4c62984b6bca1
Andrew Gregory <angregor@asu.edu>
2016-06-03 15:57:05 -0700

    pushing test changes to live phone to test more

--- newsite/app.js
+++ newsite/app.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   $(window).bind("resize", function() {
     if($(this).width() < 700) {
       $('.main').addClass('container-fluid');
+      $('#menu_opener_comb').removeClass('menu_opener_comb_desktop').addClass('menu_opener_comb_phone');
     }
     else {
       $('.main').removeClass('container-fluid');
+      $('#menu_opener_comb').removeClass('menu_opener_comb_phone').addClass('menu_opener_comb_desktop');
     }
   });
 });
